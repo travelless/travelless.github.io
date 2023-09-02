f84e8f64ecf3f11f41baa2289a0faecdc0259bb4
firstmeet <2022742378@qq.com>
2023-09-03 02:13:32 +0800

    环境变量！！最后一次！

--- docs/.vitepress/theme/components/comment.vue
+++ docs/.vitepress/theme/components/comment.vue
@@ -13,7 +13,6 @@ const config = {
   id: location.pathname,      // Ensure uniqueness and length less than 50
   distractionFreeMode: false  // Facebook-like distraction free mode
 }
-console.log(config);
 
 const gitalk = new Gitalk(config)
 
